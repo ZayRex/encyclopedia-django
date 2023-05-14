78ebec526fdae7135dd9a7fb82fa6a55d69f5811
ZayRex <31854984+ZayRex@users.noreply.github.com>
2023-05-14 23:21:50 +0100

    initial links and markdown rendering

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import markdown2
 
 from . import util
 
@@ -8,3 +10,13 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry_page(request, title):
+    page_content = markdown2.markdown(util.get_entry(title))
+    return render(request, "encyclopedia/entry_page.html", {
+        "title": title,
+        "page_content": page_content
+    })
+    # return HttpResponse(markdown2.markdown(util.get_entry(title)))
+    # return render(request, "encyclopedia/index.html", {
+    #     "entries": util.list_entries()
+    # })
