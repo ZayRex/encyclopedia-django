57b3ce6a5407193c310eb05f657483c770f8a3bb
ZayRex <31854984+ZayRex@users.noreply.github.com>
2023-05-21 03:56:47 +0100

    Implemented search function

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = "wiki"
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry_page, name="entry_page")
+    path("wiki/<str:title>", views.entry_page, name="entry_page"),
+    path("search_results", views.search_results, name="search_results")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import redirect
 import markdown2
 
 from . import util
@@ -9,13 +11,31 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+    
 
 def entry_page(request, title):
-    page_content = markdown2.markdown(util.get_entry(title))
+    entries = set(util.list_entries())
+    if title not in entries:
+        raise Http404()  
+    page_content = markdown2.markdown(util.get_entry(title))   
     return render(request, "encyclopedia/entry_page.html", {
         "title": title,
         "page_content": page_content
     })
+
+def search_results(request):
+    query = request.GET.get('q', '')
+    entries = set(util.list_entries())
+    if query in entries:
+        return redirect('wiki:entry_page', query)
+    else:
+        potential_matches = [title for title in entries if query in title]
+        return render(request, "encyclopedia/search_results.html", {
+        "query": query,
+        "potential_matches": potential_matches
+    })
+
+
     # return HttpResponse(markdown2.markdown(util.get_entry(title)))
     # return render(request, "encyclopedia/index.html", {
     #     "entries": util.list_entries()
