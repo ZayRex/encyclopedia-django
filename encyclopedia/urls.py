301b89babc7dba253a61ec8935126a1115c2c6a7
ZayRex <31854984+ZayRex@users.noreply.github.com>
2023-05-21 17:47:57 +0100

    added create new page functionality

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,5 +7,8 @@ app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry_page, name="entry_page"),
-    path("search_results", views.search_results, name="search_results")
+    path("search_results", views.search_results, name="search_results"),
+    path("create_new_page", views.CreatePage.as_view(), name="create_new_page"),
+    path("edit_page", views.EditPage.as_view(), name="edit_page"),
+    path("random_page", views.random_page, name="random_page")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.views import View
 from django.http import HttpResponse
 from django.http import Http404
 from django.shortcuts import redirect
+from django.contrib import messages
 import markdown2
+import random
 
 from . import util
 
@@ -34,6 +37,43 @@ def search_results(request):
         "query": query,
         "potential_matches": potential_matches
     })
+
+class CreatePage(View):
+
+    def get(self, request):
+        return render(request, "encyclopedia/create_new_page.html")
+        
+    def post(self, request):
+        title = request.POST.get("title")
+        entries = set(util.list_entries())
+        if title in entries:
+            messages.error(request, "The Page you are trying to create already exist. You can't overwrite it (Please try to \"Edit Page\" to modify it).")
+            return render(request, "encyclopedia/create_new_page.html")
+        page_content = request.POST.get("page_content")
+        util.save_entry(title, page_content)
+        return redirect('wiki:entry_page', title)
+
+
+def random_page(request):
+    entries = util.list_entries()
+    random_entry = random.choice(entries)
+    return redirect('wiki:entry_page', random_entry)
+
+class EditPage(View):
+
+    def get(self, request):
+        return render(request, "encyclopedia/edit_page.html")
+        
+    def post(self, request):
+        title = request.POST.get("title")
+        entries = set(util.list_entries())
+        if title in entries:
+            messages.error(request, "The Page you are trying to create already exist. You can't overwrite it (Please try to \"Edit Page\" to modify it).")
+            return render(request, "encyclopedia/create_new_page.html")
+        page_content = request.POST.get("page_content")
+        util.save_entry(title, page_content)
+        return redirect('wiki:entry_page', title)
+
 
 
     # return HttpResponse(markdown2.markdown(util.get_entry(title)))
