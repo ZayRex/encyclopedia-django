62bc6c5a92126e55eaa7afff282fc99b325ca327
ZayRex <31854984+ZayRex@users.noreply.github.com>
2023-05-23 00:08:30 +0100

    added edit page functionality

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path("wiki/<str:title>", views.entry_page, name="entry_page"),
     path("search_results", views.search_results, name="search_results"),
     path("create_new_page", views.CreatePage.as_view(), name="create_new_page"),
-    path("edit_page", views.EditPage.as_view(), name="edit_page"),
+    path("wiki/<str:title>/edit_page", views.EditPage.as_view(), name="edit_page"),
     path("random_page", views.random_page, name="random_page")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -38,6 +38,12 @@ def search_results(request):
         "potential_matches": potential_matches
     })
 
+def random_page(request):
+    entries = util.list_entries()
+    random_entry = random.choice(entries)
+    return redirect('wiki:entry_page', random_entry)
+
+
 class CreatePage(View):
 
     def get(self, request):
@@ -54,29 +60,16 @@ class CreatePage(View):
         return redirect('wiki:entry_page', title)
 
 
-def random_page(request):
-    entries = util.list_entries()
-    random_entry = random.choice(entries)
-    return redirect('wiki:entry_page', random_entry)
-
 class EditPage(View):
 
-    def get(self, request):
-        return render(request, "encyclopedia/edit_page.html")
+    def get(self, request, title):
+        page_content = util.get_entry(title)   
+        return render(request, "encyclopedia/edit_page.html",  {
+        "title": title,
+        "page_content":page_content
+    })
         
-    def post(self, request):
-        title = request.POST.get("title")
-        entries = set(util.list_entries())
-        if title in entries:
-            messages.error(request, "The Page you are trying to create already exist. You can't overwrite it (Please try to \"Edit Page\" to modify it).")
-            return render(request, "encyclopedia/create_new_page.html")
+    def post(self, request, title):
         page_content = request.POST.get("page_content")
         util.save_entry(title, page_content)
         return redirect('wiki:entry_page', title)
-
-
-
-    # return HttpResponse(markdown2.markdown(util.get_entry(title)))
-    # return render(request, "encyclopedia/index.html", {
-    #     "entries": util.list_entries()
-    # })
